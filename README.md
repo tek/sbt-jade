819a3dcdbda5d7144ba1c4aa5f6e49d324588f37
tek <torstenschmits@gmail.com>
2015-06-05 13:04:00 +0200

    use sbt-release to publish to bintray

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
-## sbt-jade
+## Description
 
-Simple syntax-only jade asset plugin for sbt-web.
+Simple syntax-only jade asset plugin for sbt-web that uses [jade4j][1].
+
+## Usage
+
+```scala
+resolvers += Resolver.url("bintray-tek-sbt-plugins",
+  url("http://dl.bintray.com/tek/sbt-plugins"))(Resolver.ivyStylePatterns)
+
+addSbtPlugin("tryp.sbt" % "sbt-jade" % "0.0.1")
+```
+
+jade files in the asset dir will be processed to html in staging.
+
+[1]: https://github.com/neuland/jade4j 'jade4j'
---
+++ project/build.scala
@@ -0,0 +1,24 @@
+import sbt._
+import Keys._
+
+import bintray._
+import BintrayKeys._
+
+object SbtJadeBuild
+extends Build
+{
+  lazy val root = (project in file("."))
+    .settings(BintrayPlugin.bintrayPublishSettings: _*)
+    .settings(
+      sbtPlugin := true,
+      organization in ThisBuild := "tryp.sbt",
+      name := "sbt-jade",
+      description := "Simple syntax-only jade asset plugin for sbt-web",
+      bintrayRepository in bintray := "sbt-plugins",
+      bintrayOrganization in bintray := None,
+      licenses += ("MIT", url("http://opensource.org/licenses/MIT")),
+      resolvers += Resolver.sonatypeRepo("releases"),
+      libraryDependencies += "de.neuland-bfi" % "jade4j" % "0.4.3",
+      addSbtPlugin("com.typesafe.sbt" % "sbt-web" % "1.2.0")
+    )
+}
